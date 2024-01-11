387fc5c2455a80c2223c1f66eebc49727aac5c6a
ERCZ <ercizhai@gmail.com>
2024-01-11 21:23:40 +0800

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import TopBar from '@/components/TopBar'
-import SideBar from '@/components/SideBar'
+import AppNavbar from '@/components/AppNavbar'
 import { Providers } from './providers'
 import '@/styles/tailwind.css'
 import '@/styles/globals.css'
@@ -7,13 +6,10 @@ import '@/styles/globals.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light">
-      <body className="flex">
-        <SideBar />
-        <div className="flex-1 flex flex-col">
-          <TopBar />
-          <main className="flex-1 p-2">
-            <Providers>{children}</Providers>
-          </main>
+      <body className="bg-default-50">
+        <AppNavbar />
+        <div className="max-w-[1024px] mx-auto p-6">
+          <Providers>{children}</Providers>
         </div>
       </body>
     </html>
---
+++ components/AppNavbar.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react'
+
+export default function AppNavbar() {
+  const nav = [
+    {
+      key: 'new',
+      href: '/stories/new',
+      text: 'New',
+    },
+    {
+      key: 'top',
+      href: '/stories/top',
+      text: 'Top',
+    },
+    {
+      key: 'best',
+      href: '/stories/best',
+      text: 'Best',
+    },
+    {
+      key: 'ask',
+      href: '/stories/ask',
+      text: 'Ask',
+    },
+    {
+      key: 'job',
+      href: '/stories/job',
+      text: 'Job',
+    },
+    {
+      key: 'show',
+      href: '/stories/show',
+      text: 'Show',
+    },
+  ]
+  const pathname = usePathname()
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>('light')
+  const toggleColorMode = () => (colorMode === 'light' ? setColorMode('dark') : setColorMode('light'))
+  useEffect(() => {
+    document.documentElement.className = colorMode
+  }, [colorMode])
+
+  return (
+    <Navbar className="w-full">
+      <NavbarBrand>
+        <h1 className="text-success text-2xl font-bold">HackerNews Explorer</h1>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        {nav.map(item => (
+          <NavbarItem key={item.key}>
+            <Link href={item.href} color={pathname === item.href ? 'success' : 'foreground'}>
+              {item.text}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <Button color="success" onClick={toggleColorMode}>
+          {colorMode === 'light' ? 'Dark' : 'Light'} Mode
+        </Button>
+      </NavbarContent>
+    </Navbar>
+  )
+}
