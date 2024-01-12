b67d6d231ee27fec3407a6697c2e35063605c4b8
ERCZ <ercizhai@gmail.com>
2024-01-12 21:48:49 +0800

    add hackernews api

---
+++ api/index.ts
@@ -0,0 +1,69 @@
+export type StoryListLabel = 'new' | 'top' | 'best' | 'ask' | 'job' | 'show'
+
+export interface Item {
+  id: number
+  deleted?: boolean
+  type: 'job' | 'story' | 'comment' | 'poll' | 'pollopt'
+  by: string
+  text: string
+  time: number
+  dead?: boolean
+  kids: number[]
+}
+
+export interface Job extends Item {
+  title: string
+  score: number
+  url: string
+}
+
+export interface Story extends Item {
+  url: string
+  score: number
+  title: string
+  descendants: number
+}
+
+export interface Comment extends Item {
+  parent: number
+}
+
+export interface Poll extends Item {
+  title: string
+  parts: number[]
+  score: number
+  descendants: number
+}
+
+export interface PollOpt extends Item {
+  poll: number
+  score: number
+}
+
+export interface User {
+  id: string
+  created: number
+  karma: number
+  about: string
+  submitted: number[]
+  delay: number
+}
+
+const baseUrl = 'https://hacker-news.firebaseio.com/v0'
+
+const request = async (url: string) => {
+  const response = await fetch(url, { next: { revalidate: 600 } })
+  return response.json()
+}
+
+export const getStories = async (label: StoryListLabel): Promise<number[]> => {
+  return request(`${baseUrl}/${label}stories.json`)
+}
+
+export const getItem = async <T extends Item>(id: number): Promise<T> => {
+  return request(`${baseUrl}/item/${id}.json`)
+}
+
+export const getUser = async (id: string): Promise<User> => {
+  return request(`${baseUrl}/user/${id}.json`)
+}
